dc6248be41123362872f45bc071fe4a82ba7ba9b
Markus Krosby <markuskrosby5@gmail.com>
2026-02-11 22:36:40 +0100

    :)

--- Bilbutikk 1 Askim System/bilbutikk1_askim/bilbutikk1_askim/settings.py
+++ Bilbutikk 1 Askim System/bilbutikk1_askim/bilbutikk1_askim/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'biler',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
