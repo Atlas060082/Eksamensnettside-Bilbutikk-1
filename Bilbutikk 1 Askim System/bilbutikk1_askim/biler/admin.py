12a5ab2c5c0724e46fce7609406fa392e1750130
Markus Krosby <markuskrosby5@gmail.com>
2026-02-11 22:38:55 +0100

    !

--- Bilbutikk 1 Askim System/bilbutikk1_askim/biler/admin.py
+++ Bilbutikk 1 Askim System/bilbutikk1_askim/biler/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import Bil, Service
 
-# Register your models here.
+class ServiceInline(admin.TabularInline):
+    model = Service
+    extra = 1
+    fields = ['type', 'beskrivelse', 'planlagt_dato', 'fullført', 'ansvarlig']
+
+@admin.register(Bil)
+class BilAdmin(admin.ModelAdmin):
+    list_display = ['regnummer', 'merke', 'modell', 'årsmodell', 'status', 'innkommet_dato']
+    list_filter = ['status', 'merke', 'drivstoff']
+    search_fields = ['regnummer', 'merke', 'modell']
+    list_editable = ['status']
+    
+    inlines = [ServiceInline]
+    
+    fieldsets = (
+        ('Bilinfo', {
+            'fields': ('merke', 'modell', 'årsmodell', 'regnummer')
+        }),
+        ('Teknisk', {
+            'fields': ('kilometerstand', 'drivstoff', 'girkasse', 'farge')
+        }),
+        ('Økonomi', {
+            'fields': ('innkjøpspris', 'salgspris')
+        }),
+        ('Status', {
+            'fields': ('status', 'innkommet_dato')
+        }),
+        ('Detaljer', {
+            'fields': ('beskrivelse', 'bilde'),
+            'classes': ('collapse',)
+        }),
+    )
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['bil', 'type', 'planlagt_dato', 'fullført', 'ansvarlig']
+    list_filter = ['fullført', 'type', 'planlagt_dato']
+    search_fields = ['bil__regnummer', 'bil__merke', 'beskrivelse']
+    list_editable = ['fullført']
+    
+    fieldsets = (
+        ('Service', {
+            'fields': ('bil', 'type', 'beskrivelse')
+        }),
+        ('Planlegging', {
+            'fields': ('planlagt_dato', 'ansvarlig')
+        }),
+        ('Kostnader', {
+            'fields': ('estimert_kostnad', 'faktisk_kostnad')
+        }),
+        ('Status', {
+            'fields': ('fullført', 'fullført_dato', 'notater')
+        }),
+    )
